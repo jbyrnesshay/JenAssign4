bf35a6d1c4f59a22f7a185e049b4e3446fab558f
jbyrnesshay <jmb464@g.harvard.edu>
2016-12-13 00:03:11 -0500

    added tap on pg1 to open popup to nav to 2, added media query style, added form items, added filter widget

--- scripts.js
+++ scripts.js
@@ -3,4 +3,24 @@ $(document).ready(function() {
 		alert('yes');
 	});
 
+
+/*tap first page get alert*/
+$('#page1').on("tap",tapHandler);
+function tapHandler(event) {
+	 
+	//alert('ha'); 
+	//window.location = document.getElementById('#funclink').href;
+	//$('#funclink').popup();
+	//$('#funclink').popup('open');
+
+	$('#popupBasic').popup('open');
+	event.preventDefault();
+	$('#popupBasic').tap(function() {
+		//event.enableDefault();
+		 $(this).unbind('#fun');
+	})
+
+
+}
+ 
 });//end ready
