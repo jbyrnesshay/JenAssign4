a9bb4563564a94a20f256e6a97454b01d64ff077
jbyrnesshay <jmb464@g.harvard.edu>
2016-12-16 21:59:10 -0500

    added  themeroller

--- scripts.js
+++ scripts.js
@@ -1,91 +1,94 @@
 $(document).ready(function() {
-	$('#second').on("pagecreate", function() {
-		alert('yes');
-	});
+	
 
-
-/*tap first page get alert*/
-$('#target').on("tap",tapHandler);
-function tapHandler(event) {
-	 
-	//alert('ha'); 
-	//window.location = document.getElementById('#funclink').href;
-	//$('#funclink').popup();
-	//$('#funclink').popup('open');
-
-	$('#popupBasic').popup('open');
-	event.preventDefault();
-	$('#popupBasic').tap(function() {
+	/*tap first page get alert*/
+	$('#target').on("tap",tapHandler);
+	function tapHandler(event) {
+		$('#popupBasic').popup('open');
+		event.preventDefault();
+		$('#popupBasic').tap(function() {
 		//event.enableDefault();
-		 $(this).unbind('#fun');
-	})
-}//end tapHanlder
+		$(this).unbind('#fun');
+		});
+	}//end tapHanlder
 
 
- $('.testcontainer').swipe( function() {
- 	//alert('ya');
- 	containvalue = $('#controlimg1').attr('src');
- 	 //alert(containvalue);
- 		$('#controlimg1').slideUp(3000, function(){
+ 	$('.testcontainer').swipe( function() {
+ 		containvalue = $('#controlimg1').attr('src');
+ 	 	$('#controlimg1').slideUp(3000, function(){
  			place2nd(containvalue);
  			if (containvalue=='test.jpg') {
- 				 
- 				 
  				$('#controlimg1').attr('src', 'images/snow.jpg');
-
- 				
- 			}
+			}
  			else { $('#controlimg1').attr('src', 'test.jpg'); }
  			$('#controlimg3').hide();
- 		}
-
- 			//$('#test').toggleClass('thing');}
- 		).slideDown(3000);
- });//end swipelef
-
-
- function place2nd(containvalue) {
-
- 	$('#controlimg2').hide();
- 	//$("#image").rotate({angle:45});
- 	$('#page2 div.ui-block-b h2').css({'visibility': 'visible', 'backgroundColor': 'aqua'});
- 	$('#controlimg2').attr('src', containvalue).slideDown(700, function() {
- 		//containvalue2 = containvalue;
- 		
- 		place3rd(containvalue)});
- }
- function place3rd(containvalue2) {
-
- 	
- 	$('#controlimg2').slideUp(function() {
- 	$('#page2 div.ui-block-b h2').css('visibility', 'hidden');
- 	$('#page2 div.ui-block-c h2').css({'visibility': 'visible', 'backgroundColor': 'aqua'});
- 	$('#controlimg3').attr('src', containvalue).slideDown(700).slideUp(700, function() {
- 		$('#page2 div.ui-block-c h2').css('visibility', 'hidden');
- 	});
- });
-}
-/*
-first() {
-	$('#control').slideUp(5000);
-}
-second() {
-	$('#control').slideDown(5000);
- }
- 	*/
- 	 
-});//end ready
-
- 	/*
-	function down() {
-		//new = '/water-lily.jpeg'
-		//$('#test').attr('src', new);
-		$('#control').slideDown(5000);
+ 		}).slideDown(3000);
+ 	});//end swipe
+function place2nd(containvalue) {
+		$('#controlimg2').hide();
+ 		$('#page2 div.ui-block-b h2').css({'visibility': 'visible', 'backgroundColor': 'aqua'});
+ 		$('#controlimg2').attr('src', containvalue).slideDown(700, function() {
+ 			place3rd(containvalue)});
 		}
+ 		
+ 		function place3rd(containvalue2) {
+			$('#controlimg2').slideUp(function() {
+ 			$('#page2 div.ui-block-b h2').css('visibility', 'hidden');
+ 			$('#page2 div.ui-block-c h2').css({'visibility': 'visible', 'backgroundColor': 'aqua'});
+ 			$('#controlimg3').attr('src', containvalue).slideDown(700).slideUp(700, function() {
+ 			$('#page2 div.ui-block-c h2').css('visibility', 'hidden');
+ 				});
+ 		});
 
-		function ui() {
-		new = 'water-lily.jpeg'
-		alert(new);
-		//$('#control').attr('src', new);
-		$('#control').slideDown(5000);
-	}*/
+ 			}
+
+maincolor = $('div.ui-grid-b').css("backgroundColor");
+ 
+$('#slider-flip-m').on("change", function() {
+		state = $('#slider-flip-m').val();
+		if (state == 'off') {
+			 
+			$('div.ui-grid-b').css("backgroundColor", "black");
+		}
+		else $('div.ui-grid-b').css("backgroundColor", maincolor)
+		});
+
+$('input[type="radio"][name=radiobutton]').change(function(){
+	val = $(this).val();
+	//alert(val);
+	if (val == 'blue') {
+		$('div.ui-grid-b').css("backgroundColor", "blue");
+
+	}
+	else if (val == 'red') {
+	$('div.ui-grid-b').css("backgroundColor", "red");
+
+	}
+	else $('div.ui-grid-b').css("backgroundColor", maincolor);
+});
+
+$('#select-background').change(function(){
+	val = $(this).val();
+	//alert(val);
+	if(val=='pink') {
+		$('div.ui-grid-b').css("backgroundColor", "pink");
+
+	}
+	else if (val == "purple") {
+		$('div.ui-grid-b').css("backgroundColor", "purple");
+	}
+	else if (val == 'green') {
+		$('div.ui-grid-b').css("backgroundColor", "green");
+	}
+	else if (val == 'brown') {
+		$('div.ui-grid-b').css("backgroundColor", "brown");
+
+	}
+	else if (val == 'orange') {
+		$('div.ui-grid-b').css("backgroundColor", 'orange');
+	}
+	else $('div.ui-grid-b').css("backgroundColor", maincolor);
+});
+
+	});//end ready
+ 	 
